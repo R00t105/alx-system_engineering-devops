df8b3382ff1386abc2754de5673d52e9e068e348
R00t105 <moclaymo@gmail.com>
2024-08-15 07:54:14 +0300

    alx tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import sys
 def top_ten(subreddit):
     """ Returns: top ten post titles
         or None if queried subreddit is invalid """
-    headers = {'User-Agent': 'xica369'}
+    headers = {'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
     response = requests.get(url, headers=headers, allow_redirects=False,
