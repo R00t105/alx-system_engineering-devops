3a0f9b03c73a6bd7ceaff854cc762ff303bd6476
R00t105 <moclaymo@gmail.com>
2024-08-15 07:48:21 +0300

    alx tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the top 10 hot posts for a given subreddit."""
     headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
